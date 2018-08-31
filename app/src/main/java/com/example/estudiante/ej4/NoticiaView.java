cc54fcea33b00dc78b55f2e67e22c540b83fcb5e
carolinac98 <carolinac98@hotmail.com>
2018-08-31 15:52:36 -0500

    Initial commit

---
+++ app/src/main/java/com/example/estudiante/ej4/NoticiaView.java
@@ -0,0 +1,13 @@
+package com.example.estudiante.ej4;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NoticiaView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_noticia_view);
+    }
+}
